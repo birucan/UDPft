67667a8988da111ae8f0dfe4259684a937ddfe36
ndiaz9 <na.diaz@uniandes.edu.co>
2020-10-27 21:56:06 -0500

    hash y log

--- ftclient.py
+++ ftclient.py
@@ -1,33 +1,62 @@
 import socket
 import select
 import time
+import hashlib
+
+def hash_file(filename):
+   h = hashlib.sha1()
+   with open(filename,'rb') as file:
+       chunk = 0
+       while chunk != b'':
+           chunk = file.read(1024)
+           h.update(chunk)
+   return h.hexdigest()
+
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 host = "127.0.0.1"
 port = 9999
 buf =1024
 
-
 sock.bind((host, port))
 
 while (True):
     data, addr = sock.recvfrom(1024)
-
     if (data):
-        print ("File name:", data)
+        print ("Path:", data)
         file_name = "archivo.dummy"
 
     f = open(file_name, 'wb')
     sock.sendto(data, addr)
     data = (data.decode("ascii")+str((time.time()*1000))).encode("ascii")
-    sock.sendto(data, addr)
+
+    hash_recibido, addr = sock.recvfrom(1024)
+    print("Hash recibido: {}".format(hash_recibido))
     while (True):
         ##espera que todos los clientes esten activos
-        if(sock.recvfrom(1024)=="ready".encode("ascii")):
-            while (True):
-                ready = select.select([sock], [], [], 0.2)
-                if ready[0]:
-                    data, addr = sock.recvfrom(1024)
-                    f.write(data)
-                else:
-                    f.close()
-                    break
+        ready = select.select([sock], [], [], 3)
+        if ready[0]:
+            data, addr = sock.recvfrom(1024)
+            f.write(data)
+        else:
+            f.close()
+            break
+    fin = time.time()
+    print('Descarga terminada')
+    hash_local = bytes(hash_file("archivo.dummy"),'ascii')
+    print("Hash local: {}".format(hash_local))
+    if hash_local == hash_recibido:
+        print("Hash Correcto - Enviando Recibido")
+        sock.sendto("Recibido".encode(),addr)
+        inicio, addr = sock.recvfrom(1024)
+        print(inicio)
+        tiempo = round(fin-float(inicio),2)
+        print("Tiempo total: {} segundos".format(tiempo))
+        sock.sendto(str(tiempo).encode()+b'\n',addr)
+    else:
+        print("Error en el Hash")
+        sock.sendto("Error de hash".encode(),addr)
+        inicio, addr = sock.recvfrom(1024)
+        print(inicio)
+        tiempo = round(fin-float(inicio),2)
+        print("Tiempo total: {} segundos".format(tiempo))
+        sock.sendto(str(tiempo).encode()+b'\n',addr)
--- ftserver.py
+++ ftserver.py
@@ -1,12 +1,20 @@
 
 import socket
 import sys
+import threading
+import ntpath
+import os
+import hashlib
+import time
+import queue
+from datetime import datetime
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 host = "127.0.0.1"
 port = 9999
 buf =1024
 address = (host,port)
+fecha = datetime.now().strftime("%d-%m-%Y_%H_%M_%S")
 
 print("Archivos:")
 print(" (1) 1.dummy -   100mb")
@@ -19,39 +27,62 @@ if(foo==2):
     filePath = './archivos/2.dummy'
 
 numClientes = int(input("Ingrese numero de clientes: "))
-
 sock.sendto(filePath.encode('ascii'),address)
-clientesLlegados=0
-data="yes"
-while(True):
-    print('llego cliente espernado el resto', numClientes-clientesLlegados)
-    if(data==sock.recvfrom(1024)):
-        data, addr = sock.recvfrom(1024)
-    else:
-        clientesLlegados=clientesLlegados+1
-        data, addr = sock.recvfrom(1024)
 
-    #mensaje="Hola soy el server, te voy a enviar un archivo ome"
-    #sender= sock.sendto(mensaje.encode(), addr)
-    #data, addr = sock.recvfrom(1024)
+def hash_file(filename):
+   h = hashlib.sha1()
+   with open(filename,'rb') as file:
+       chunk = 0
+       while chunk != b'':
+           chunk = file.read(1024)
+           h.update(chunk)
+   return h.hexdigest()
+
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+while(True):
+    data, addr = sock.recvfrom(1024)
+        #mensaje="Hola soy el server, te voy a enviar un archivo ome"
+        #sender= sock.sendto(mensaje.encode(), addr)
+        #data, addr = sock.recvfrom(1024)
+
+    filename = path_leaf(filePath)
+    filesize = f'{os.path.getsize(filePath)}'
+
 
     recibido = data
     print("el cliente esta conectado con el mensaje: "+ recibido.decode('ascii'))
-    if(numClientes==clientesLlegados):
+    sock.sendto(hash_file(filePath).encode(),address)
 
-        f=open(filePath,"rb")
-        data = f.read(buf)
-        while (data):
-            if(sock.sendto(data,address)):
-                print ("mandando archivo ...")
-                data = f.read(buf)
+    log = fecha + " | " + filename + " | " + str(round((int(filesize)/1024),2)) + "KB"
+    inicio = time.time()
+    
+    f=open(filePath,"rb")
+    data = f.read(buf)
+    while (data):
+        if(sock.sendto(data,address)):
+            print ("mandando archivo ...")
+            data = f.read(buf)
+    print("transferencia terminada")
+    client_recibido = sock.recv(buf).decode()
+    
+    if client_recibido == "Recibido":
+        print("Enviado con exito a cliente")
+        sock.sendto(str(inicio).encode(),address)
+        tiempo = float(sock.recv(buf).decode())
+        log = log + " | Tiempo: " + str(tiempo) + "s | Exitoso\n"
+    else:
+        print("Errores en el envio a cliente")
+        print(str(inicio).encode())
+        sock.sendto(str(inicio),address)
+        tiempo = float(sock.recv(buf).decode())
+        log = log + " | Tiempo: " + str(tiempo) + "s | Error\n"
 
-
-
-#f=open(file_name,"rb")
-#data = f.read(buf)
-#while (data):
-#    if(s.sendto(data,address)):
-##        data = f.read(buf)
+    log_file = open(fecha+".txt",'a')
+    log_file.write(log)
+    log_file.close()
 sock.close()
 f.close()
